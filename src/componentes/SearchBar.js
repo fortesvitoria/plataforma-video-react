5d20e7867edd35bb99c5e65b2b537be7eb0a1e23
Vitoria Fortes <fortesvitoria@gmail.com>
2024-04-02 09:34:19 -0300

    aula 6

--- src/componentes/SearchBar.js
+++ src/componentes/SearchBar.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react'
 import { Segment, Input } from 'semantic-ui-react'
 
 class SearchBar extends Component {
+
+    pesquisaTermo = e => {
+        //codigo da tecla enter é 13
+        if (e.keyCode === 13) {
+            const termo = e.target.value
+            console.log(termo)
+        }
+    }
+
     render() {
         return (
             <div className='search-bar'>
                 <Segment stacked>
-                    <Input icon='search' size='large' placeholder='Search...'></Input>
+                    <Input icon='search' onKeyDown={(e) => this.pesquisaTermo(e)} size='large' placeholder='Search...'></Input>
                 </Segment>
             </div>
         )
